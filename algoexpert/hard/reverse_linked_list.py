560b2b3d0e37d2ce0c7f90f003d5c1ded09006fe
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    pylint: fix style

--- algoexpert/hard/reverse_linked_list.py
+++ algoexpert/hard/reverse_linked_list.py
@@ -1,12 +1,20 @@
-class LinkedList:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+"""
+Write a function that takes in the head of a Singly Linked List, reverses the
+list in place (i.e., doesn't create a brand new list), and returns its new head.
+
+Each LinkedList node has an integer value as well as a next node pointig to the
+next node in the list or to None if it's the tail of the list.
+
+You can assume that the input Linked List will always have at least one node; in
+other words, the head will never be None.
+"""
 
 
-# O(n) O(1)
-def reverseLinkedList(head):
-    prev, next = None, head
-    while next:
-        next.next, prev, next = prev, next, next.next
+def reverse_linked_list(head):
+    """
+    O(n) O(1)
+    """
+    prev, curr = None, head
+    while curr:
+        curr.next, prev, curr = prev, curr, curr.next
     return prev
