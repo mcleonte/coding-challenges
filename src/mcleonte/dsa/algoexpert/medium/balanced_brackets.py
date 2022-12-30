8bb32e40e55fa0f254e23546b0391b95fe8945fe
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:03 +0200

    init commit

---
+++ src/mcleonte/dsa/algoexpert/medium/balanced_brackets.py
@@ -0,0 +1,27 @@
+"""
+Write a function that takes in a string made up of brackets (, [, {, ), ] and }
+and other optional characters. The function should return a boolean representing
+whether the string is balanced with regards to brackets.
+
+A string is said to be balanced if it has as many opening brackets of a certain
+type as it has closing brackets of that type and if no bracket is unmatched.
+Note that an opening bracket can't match a corresponding closing bracket that
+comes before it, and similarly, a closing bracket can't match a corresponding
+opening bracket that comes after it. Also, brackets can't overlap each other as
+in [[(]).
+"""
+
+
+def balanced_brackets(string: str) -> bool:
+  """
+  O(n) O(n)
+  """
+  stack, pairs = [], {"(": ")", "[": "]", "{": "}"}
+  opening, closing = set("{[("), set(")]}")
+  for char in string:
+    if char in opening:
+      stack.append(char)
+    elif char in closing:
+      if not stack or char != pairs[stack.pop()]:
+        return False
+  return len(stack) == 0
