ae337bb378e597ddb0e2f9328783f793e4647039
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/not_boring_movies.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/not_boring_movies.sql
@@ -1,7 +1,11 @@
 -- https://leetcode.com/problems/not-boring-movies/
 -- https://leetcode.com/problems/not-boring-movies/discuss/2837414/MySQL-or-greater94-fastest-solution-with-Bitwise-AND
-SELECT *
-FROM Cinema
+SELECT
+  id,
+  movie,
+  description,
+  rating
+FROM cinema
 WHERE id & 1 = 1
-AND description <> 'boring'
+  AND description != 'boring'
 ORDER BY rating DESC
