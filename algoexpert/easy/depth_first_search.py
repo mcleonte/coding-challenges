1d3862501e81348cc25ebd369dcd51efb01fb732
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:03 +0200

    add docstring

--- algoexpert/easy/depth_first_search.py
+++ algoexpert/easy/depth_first_search.py
@@ -1,4 +1,14 @@
-# O(n+e) O(e) | n = number of nodes, e = number of edges
+"""
+You're given a Node class that has a name and an array of optional children
+nodes. When put together, nodes form an acyclic tree-like structure.
+
+Implement the depthFirstSearch method on the Node class, which takes in an empty
+array, traverses the tree using the Depth-first Search approach, specifically
+navigating the tree from left to right, stores all of the nodes' names in the
+input array, and returns it.
+"""
+
+
 class Node:
 
   def __init__(self, name):
@@ -10,6 +20,9 @@ class Node:
     return self
 
   def depthFirstSearch(self, array):
+    """
+    O(n+e) O(e) | n = number of nodes, e = number of edges
+    """
     array.append(self.name)
     for node in self.children:
       node.depthFirstSearch(array)
