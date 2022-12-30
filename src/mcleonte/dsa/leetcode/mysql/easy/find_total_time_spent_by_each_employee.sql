1c9da1f066c661aacd7231a536029ab4a5db0c85
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/find_total_time_spent_by_each_employee.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/find_total_time_spent_by_each_employee.sql
@@ -1,7 +1,7 @@
 -- https://leetcode.com/problems/find-total-time-spent-by-each-employee/
 SELECT
-    event_day as day,
-    emp_id,
-    SUM(out_time - in_time) as total_time
-FROM Employees
+  event_day AS day,
+  emp_id,
+  SUM(out_time - in_time) AS total_time
+FROM employees
 GROUP BY emp_id, event_day
