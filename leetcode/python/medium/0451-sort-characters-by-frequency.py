57596812db0535af91244b440419b86e3714ab7a
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    change O(nlongn) to O(n), add daily challenge post link

--- leetcode/python/medium/0451-sort-characters-by-frequency.py
+++ leetcode/python/medium/0451-sort-characters-by-frequency.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/sort-characters-by-frequency/
-# https://leetcode.com/problems/sort-characters-by-frequency/discuss/2871234/Python-or-O(nlogn)-O(n)-or-shortest-one-liner
-# O(nlogn) O(n)
+# https://leetcode.com/problems/sort-characters-by-frequency/discuss/2871158/daily-leetcoding-challenge-december-day-3/1703882
+# https://leetcode.com/problems/sort-characters-by-frequency/discuss/2871158/Daily-LeetCoding-Challenge-December-Day-3/1703524
+# O(n) O(n)
 class Solution:
     def frequencySort(self, s: str) -> str:
         return "".join(k * v for k, v in Counter(s).most_common())
