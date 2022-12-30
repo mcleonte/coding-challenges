b04ff4579ecf631a6e07b4182dc379354b81b48d
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/user_activity_for_the_past_30_days_i.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/user_activity_for_the_past_30_days_i.sql
@@ -1,8 +1,8 @@
 -- https://leetcode.com/problems/user-activity-for-the-past-30-days-i/
 -- https://leetcode.com/problems/user-activity-for-the-past-30-days-i/discuss/2825169/MySQL-or-Single-WHERE-clause%3A-DATEDIFF-...-BETWEEN-0-AND-29
 SELECT
-    activity_date as day,
-    COUNT(DISTINCT user_id) as active_users
-FROM Activity
-WHERE DATEDIFF('2019-07-27',activity_date) BETWEEN 0 AND 29
+  activity_date AS day,
+  COUNT(DISTINCT user_id) AS active_users
+FROM activity
+WHERE DATEDIFF('2019-07-27', activity_date) BETWEEN 0 AND 29
 GROUP BY day
