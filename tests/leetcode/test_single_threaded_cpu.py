597ca09d232d5ec36dcfe7c5a129f8c7aa0afe6b
Mihail Costin Leonte <git@mcleonte.com>
2022-12-31 00:33:43 +0200

    add 2 tests

--- tests/leetcode/test_single_threaded_cpu.py
+++ tests/leetcode/test_single_threaded_cpu.py
@@ -8,3 +8,13 @@ from mcleonte.dsa.leetcode.python.medium.single_threaded_cpu import get_order
 def test_1():
   arg = [[5, 2], [7, 2], [9, 4], [6, 3], [5, 10], [1, 1]]
   assert get_order(arg) == [5, 0, 1, 3, 2, 4]
+
+
+def test_2():
+  arg = [[1, 2], [2, 4], [3, 2], [4, 1]]
+  assert get_order(arg) == [0, 2, 3, 1]
+
+
+def test_3():
+  arg = [[7, 10], [7, 12], [7, 5], [7, 4], [7, 2]]
+  assert get_order(arg) == [4, 3, 2, 0, 1]
