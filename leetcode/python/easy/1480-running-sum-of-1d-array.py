1cee670e5256b823f2e0d506f1f3845aac165cb8
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:01 +0200

    init commit

---
+++ leetcode/python/easy/1480-running-sum-of-1d-array.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/running-sum-of-1d-array/
+# O(n) O(1)
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        return nums
