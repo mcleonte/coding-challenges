f6a21331a81c0fac3678bc28cf1c0a228dafab44
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    reformat indent

--- src/mcleonte/dsa/leetcode/mysql/medium/exchange_seats.sql
+++ src/mcleonte/dsa/leetcode/mysql/medium/exchange_seats.sql
@@ -1,7 +1,7 @@
 -- https://leetcode.com/problems/exchange-seats/submissions/
 -- https://leetcode.com/problems/exchange-seats/discuss/2860927/MySQL-or-greater98-Shortest-solution-with-pure-math-no-IF-CASE-UNION-WHERE
 SELECT
-    id - 1 + (id & 1) * (2 - (id = (SELECT COUNT(id) FROM Seat))) AS id,
-    student
-FROM Seat
+  student,
+  id - 1 + (id & 1) * (2 - (id = (SELECT COUNT(id) FROM seat))) AS id
+FROM seat
 ORDER BY id
