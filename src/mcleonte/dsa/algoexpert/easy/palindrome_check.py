3a82230092f55dc9970408d063d3af9ad092fada
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:03 +0200

    change style, add docstring

--- src/mcleonte/dsa/algoexpert/easy/palindrome_check.py
+++ src/mcleonte/dsa/algoexpert/easy/palindrome_check.py
@@ -1,16 +1,31 @@
-# O(n) O(1)
-def isPalindrome(string):
+"""
+Write a function that takes in a non-empty string and that returns a boolean
+representing whether the string is a palindrome.
+
+A palindrome is defined as a string that's written the same forward and
+backward. Note that single-character strings are palindromes.
+"""
+
+
+def is_palindrome(string):
+  """
+  O(n) O(1)
+  """
   for i in range(len(string) // 2):
     if string[i] != string[-i - 1]:
       return False
   return True
 
 
-# O(n) O(1)
-def isPalindrome2(string):
+def is_palindrome_2(string):
+  """
+  O(n) O(1)
+  """
   return all(string[i] == string[-i - 1] for i in range(len(string) // 2))
 
 
-# O(n) O(n)
-def isPalindrome3(string):
+def is_palindrome_3(string):
+  """
+  O(n) O(n)
+  """
   return string == string[::-1]
