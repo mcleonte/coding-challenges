2de38a5a3e9cc19a62516f4b273ae29f453275b1
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:03 +0200

    change loop method

--- src/mcleonte/dsa/algoexpert/easy/insertion_sort.py
+++ src/mcleonte/dsa/algoexpert/easy/insertion_sort.py
@@ -10,11 +10,9 @@ def insertion_sort(arr: List[int]):
   """
   O(n^2) O(1)
   """
-  i = 1
-  while i < len(arr):
+  for i in range(1, len(arr)):
     for j in range(i - 1, -1, -1):
       if arr[j + 1] >= arr[j]:
         break
       arr[j + 1], arr[j] = arr[j], arr[j + 1]
-    i += 1
   return arr
