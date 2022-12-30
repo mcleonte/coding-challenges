be763d28a2d6b3116a00a472f7e9dede6bbf101a
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    change style

--- leetcode/python/medium/sort_characters_by_frequency.py
+++ leetcode/python/medium/sort_characters_by_frequency.py
@@ -1,8 +1,14 @@
-# https://leetcode.com/problems/sort-characters-by-frequency/
-# https://leetcode.com/problems/sort-characters-by-frequency/discuss/2871158/daily-leetcoding-challenge-december-day-3/1703882
-# https://leetcode.com/problems/sort-characters-by-frequency/discuss/2871158/Daily-LeetCoding-Challenge-December-Day-3/1703524
-# O(n) O(n)
-class Solution:
+"""
+https://leetcode.com/problems/sort-characters-by-frequency/
+https://leetcode.com/problems/sort-characters-by-frequency/discuss/2871158/daily-leetcoding-challenge-december-day-3/1703882
+https://leetcode.com/problems/sort-characters-by-frequency/discuss/2871158/Daily-LeetCoding-Challenge-December-Day-3/1703524
+"""
 
-  def frequencySort(self, s: str) -> str:
-    return "".join(k * v for k, v in Counter(s).most_common())
+from collections import Counter
+
+
+def frequency_sort(s: str) -> str:
+  """
+  O(n) O(n)
+  """
+  return "".join(k * v for k, v in Counter(s).most_common())
