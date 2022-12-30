977dca66371045ad9520e8e824c1bbec24d76a88
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    init commit

---
+++ algoexpert/easy/tournament-winner.py
@@ -0,0 +1,15 @@
+# O(n) O(n)
+def tournamentWinner(competitions, results):
+    totals = {}
+    for c, r in zip(competitions, results):
+        w = c[1 - r]
+        try:
+            totals[w] += 3
+        except KeyError:
+            totals[w] = 3
+    winner, score = "", 0
+    for k, v in totals.items():
+        if v > score:
+            score = v
+            winner = k
+    return winner
