c94808f9d75bc7d4b9bcf0733804fb6368605802
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    init commit

---
+++ leetcode/python/easy/1920-build-array-from-permutation.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/build-array-from-permutation/
+# https://leetcode.com/problems/build-array-from-permutation/discuss/2867917/Python-or-O(n)-O(1)-or-Very-short-solution-with-detailed-description
+class Solution:
+
+    # O(n) O(1)
+    def buildArray(self, nums: List[int]) -> List[int]:
+        """
+        For O(1) space it is required to store 2 numbers, nums[i] and nums[nums[i]],
+        as a single number at index i. Thus, nums[nums[i]] can be stored as a quotient,
+        while the original value nums[i] can "separately" be kept as a remainder,
+        where the divisor is len(nums).
+
+        As such, after the first pass, each number is modified as n = n + nums[n] * l.
+
+        However, this operation might be applied at index n before being applied at
+        index i, and we need only the original value at index n to store it as a quotient
+        at index i. By following the same logic, the original value at index n is obtained
+        by extracting only the remainder from nums[n].
+
+        Therefore, the first pass needs to be modified to n = n + ( nums[n] % l ) * l.
+
+        Finally, after the second pass, each number has its remainder removed,
+        the original value, keeping only the quotient, the new value.
+        """
+        l = len(nums)
+        for i in range(l):
+            nums[i] += nums[nums[i]] % l * l
+        for i in range(l):
+            nums[i] //= l
+        return nums
+
+    # O(n) O(n)
+    def buildArray2(self, nums: List[int]) -> List[int]:
+        return [nums[n] for n in nums]
