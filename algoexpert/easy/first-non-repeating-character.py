724ce342a6477979f0970c5ff2ee497ab17d30d1
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    init commit

---
+++ algoexpert/easy/first-non-repeating-character.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+# O(n) O(n)
+def firstNonRepeatingCharacter(string):
+    r = Counter(string)
+    for i, c in enumerate(string):
+        if r[c] == 1:
+            return i
+    return -1
