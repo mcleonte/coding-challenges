564756c7a5d378441010e7f1b4ab441eb143d7ba
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:01 +0200

    init commit

---
+++ leetcode/mysql/easy/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/user-activity-for-the-past-30-days-i/
+SELECT
+    activity_date as day,
+    COUNT(DISTINCT user_id) as active_users
+FROM Activity
+WHERE DATEDIFF('2019-07-27',activity_date) BETWEEN 0 AND 29
+GROUP BY day
