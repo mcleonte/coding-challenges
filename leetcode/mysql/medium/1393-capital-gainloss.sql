fcae05353893ff1ec3d7009da166a03290c43cc0
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:01 +0200

    init commit

---
+++ leetcode/mysql/medium/1393-capital-gainloss.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/capital-gainloss/
+-- https://leetcode.com/problems/capital-gainloss/discuss/2825394/MySQL-or-greater97-with-simplest-solution-possible-or-use-IF-not-CASE
+SELECT
+    stock_name,
+    SUM(IF(operation='Sell',price,-price)) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
