3148c77321e359c647c825e9f2d67b6a94994a19
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    init commit

---
+++ leetcode/mysql/easy/0620-not-boring-movies.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/not-boring-movies/
+-- https://leetcode.com/problems/not-boring-movies/discuss/2837414/MySQL-or-greater94-fastest-solution-with-Bitwise-AND
+SELECT *
+FROM Cinema
+WHERE id & 1 = 1
+AND description <> 'boring'
+ORDER BY rating DESC
