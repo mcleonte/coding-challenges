cefd6cc3f5e3452440bb80aec6f1cbf9018ef57c
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    init commit

---
+++ leetcode/python/easy/range_sum_of_bst.py
@@ -0,0 +1,34 @@
+"""
+https://leetcode.com/problems/range-sum-of-bst/
+
+Given the root node of a binary search tree and two integers low and high,
+return the sum of values of all nodes with a value in the inclusive range
+[low, high]. All Node.val are unique.
+"""
+
+from typing import Optional
+
+from .. import TreeNode
+
+
+class Solution:
+    def range_sum_bst(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        return (
+            0
+            if not root
+            else (root.val if low <= root.val <= high else 0)
+            + (self.range_sum_bst(root.left, low, high) if root.val > low else 0)
+            + (self.range_sum_bst(root.right, low, high) if root.val < high else 0)
+        )
+
+    def range_sum_bst_2(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if not root:
+            return 0
+        out, val = 0, root.val
+        if low <= val <= high:
+            out += val
+        if low < val:
+            out += self.range_sum_bst_2(root.left, low, high)
+        if val < high:
+            out += self.range_sum_bst_2(root.right, low, high)
+        return out
