50242b3452860be6826c25db9915d598a7ebd4e2
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    init commit

---
+++ algoexpert/medium/three-number-sum.py
@@ -0,0 +1,31 @@
+# O(n^2) O(n)
+def threeNumberSum(array, targetSum):
+    array.sort()  # O(nlogn)
+    out, l = [], len(array)
+    for i in range(l - 1):
+        j, k, t = i + 1, l - 1, targetSum - array[i]
+        while j < k:
+            s = array[j] + array[k]
+            if s == t:
+                out.append([array[i], array[j], array[k]])
+                j += 1
+                k -= 1
+            elif s < t:
+                j += 1
+            else:
+                k -= 1
+    return out
+
+
+# O(n^3) O(n)
+def threeNumberSum(array, targetSum):
+    array.sort()  # O(nlogn)
+    out, l = [], len(array)
+    for i in range(l - 2):
+        t = targetSum - array[i]
+        for j in range(i + 1, l - 1):
+            t2 = t - array[j]
+            for k in range(j + 1, l):
+                if array[k] == t2:
+                    out.append([array[i], array[j], array[k]])
+    return out
