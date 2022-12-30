a0590442297cad766739d5f031ee0a0ed0ace9f3
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting and filename typo

--- src/mcleonte/dsa/leetcode/mysql/easy/patients_with_a_condition.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/patients_with_a_condition.sql
@@ -1,5 +1,8 @@
 -- https://leetcode.com/problems/patients-with-a-condition/
 -- https://leetcode.com/problems/patients-with-a-condition/discuss/2831333/MySQL-or-90-without-LIKE-clause
-SELECT *
-FROM Patients
+SELECT
+  patient_id,
+  patient_name,
+  conditions
+FROM patients
 WHERE conditions REGEXP '\\bDIAB1'
