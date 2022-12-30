00702680ade31eee304ed359777155a54068a9bf
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    change style

--- leetcode/python/medium/three_sum.py
+++ leetcode/python/medium/three_sum.py
@@ -1,15 +1,19 @@
-# https://leetcode.com/problems/3sum/
-class Solution:
+"""
+https://leetcode.com/problems/3sum/
+"""
 
-  def threeSum(self, nums: List[int]) -> List[List[int]]:
-    out = {}
-    for i, n in enumerate(nums):
-      target = -n
-      s = {}
-      for j, m in enumerate(nums[i + 1:]):
-        if target - m in s:
-          f = frozenset({n, m, target - m})
-          if f not in out:
-            out[f] = [n, m, target - m]
-        s[m] = j
-    return out.values()
+from typing import List
+
+
+def three_sum(nums: List[int]) -> List[List[int]]:
+  out = {}
+  for i, n in enumerate(nums):
+    target = -n
+    s = {}
+    for j, m in enumerate(nums[i + 1:]):
+      if target - m in s:
+        f = frozenset({n, m, target - m})
+        if f not in out:
+          out[f] = [n, m, target - m]
+      s[m] = j
+  return out.values()
