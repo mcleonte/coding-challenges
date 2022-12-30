9454de5fa7d6bbbef37afedd01b7f4405b139bfd
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/customers_who_never_order.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/customers_who_never_order.sql
@@ -1,7 +1,7 @@
 -- https://leetcode.com/problems/customers-who-never-order/
 -- https://leetcode.com/problems/customers-who-never-order/discuss/2831261/MySQL-or-98.77-407ms
-SELECT c.name AS Customers
-FROM Customers c
-LEFT JOIN Orders o
-ON c.id = o.customerId
-WHERE o.customerId IS NULL
+SELECT customers.name AS customers
+FROM customers
+LEFT JOIN orders
+  ON customers.id = orders.customerid
+WHERE orders.customerid IS NULL
