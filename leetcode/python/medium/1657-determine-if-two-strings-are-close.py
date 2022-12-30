40f51a48623e89bd4e0e8b7cb8c5df6b071577e1
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    init commit

---
+++ leetcode/python/medium/1657-determine-if-two-strings-are-close.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/determine-if-two-strings-are-close/
+# https://leetcode.com/problems/determine-if-two-strings-are-close/discuss/2871072/Python-or-O(n)-O(n)-or-2-liner-and-equivalent-expanded-versions-or-no-sorting
+class Solution:
+
+    # O(n) O(n)
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        c1, c2 = Counter(word1), Counter(word2)
+        return c1.keys() == c2.keys() and Counter(c1.values()) == Counter(c2.values())
+
+    # O(n) O(n) - equivalent expanded version
+    def closeStrings2(self, word1: str, word2: str) -> bool:
+        dflt = lambda: 0
+        d1 = defaultdict(dflt)
+        d2 = defaultdict(dflt)
+        v1 = defaultdict(dflt)
+        v2 = defaultdict(dflt)
+        for k in word1:
+            d1[k] += 1
+        for k in word2:
+            d2[k] += 1
+        for k in d1.values():
+            v1[k] += 1
+        for k in d2.values():
+            v2[k] += 1
+        return d1.keys() == d2.keys() and v1 == v2
