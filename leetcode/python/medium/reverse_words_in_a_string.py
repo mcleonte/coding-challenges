f4fa7618508ffd3cd85c3105db9f134fc8a6d4f3
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    change style

--- leetcode/python/medium/reverse_words_in_a_string.py
+++ leetcode/python/medium/reverse_words_in_a_string.py
@@ -1,21 +1,29 @@
-# https://leetcode.com/problems/reverse-words-in-a-string/
-class Solution:
+"""
+https://leetcode.com/problems/reverse-words-in-a-string/
+"""
 
-  def reverseWords(self, s: str) -> str:
 
-    # one-liner "cheat"
-    # return " ".join(s.split()[::-1])
+def reverse_words(s: str) -> str:
+  """
+  O(n) O(n)
+  """
+  return " ".join(s.split()[::-1])
 
-    # no split or reverse
-    out = []
-    i = len(s) - 1
-    while i > -1:
-      if s[i] == " ":
-        i -= 1
-      else:
-        j = i - 1
-        while j > -1 and s[j] != " ":
-          j -= 1
-        out.append(s[j + 1:i + 1])
-        i = j - 1
-    return " ".join(out)
+
+def reverse_words_2(s: str) -> str:
+  """
+  Same as the one-line, without any split or reverse
+  O(n) O(n)
+  """
+  out = []
+  i = len(s) - 1
+  while i > -1:
+    if s[i] == " ":
+      i -= 1
+    else:
+      j = i - 1
+      while j > -1 and s[j] != " ":
+        j -= 1
+      out.append(s[j + 1:i + 1])
+      i = j - 1
+  return " ".join(out)
