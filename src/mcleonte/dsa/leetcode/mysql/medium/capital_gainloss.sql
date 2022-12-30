4429b60d80f1fb60c7a1f02ad0a800b464f226c5
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    reformat indent

--- src/mcleonte/dsa/leetcode/mysql/medium/capital_gainloss.sql
+++ src/mcleonte/dsa/leetcode/mysql/medium/capital_gainloss.sql
@@ -1,7 +1,7 @@
 -- https://leetcode.com/problems/capital-gainloss/
 -- https://leetcode.com/problems/capital-gainloss/discuss/2825394/MySQL-or-greater97-with-simplest-solution-possible-or-use-IF-not-CASE
 SELECT
-    stock_name,
-    SUM(IF(operation='Sell',price,-price)) AS capital_gain_loss
-FROM Stocks
+  stock_name,
+  SUM(IF(operation = 'Sell', price, -price)) AS capital_gain_loss
+FROM stocks
 GROUP BY stock_name
