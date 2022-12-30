9784fb1ab7c7ecc7f9b0dac4908fa19a86b55cfd
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:03 +0200

    change style, add docstring

--- leetcode/python/easy/running_sum_of_1d_array.py
+++ leetcode/python/easy/running_sum_of_1d_array.py
@@ -1,8 +1,19 @@
-# O(n) O(1)
-# https://leetcode.com/problems/running-sum-of-1d-array/
-class Solution:
+"""
+https://leetcode.com/problems/running-sum-of-1d-array/
 
-  def runningSum(self, nums: List[int]) -> List[int]:
-    for i in range(1, len(nums)):
-      nums[i] += nums[i - 1]
-    return nums
+Given an array nums. We define a running sum of an array as
+runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+"""
+
+from typing import List
+
+
+def running_sum(nums: List[int]) -> List[int]:
+  """
+  O(n) O(1)
+  """
+  for i in range(1, len(nums)):
+    nums[i] += nums[i - 1]
+  return nums
