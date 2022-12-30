3eeac47987efc2786b6afebf53405137df96863e
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:03 +0200

    change style

--- leetcode/python/easy/remove_duplicates_from_sorted_list.py
+++ leetcode/python/easy/remove_duplicates_from_sorted_list.py
@@ -1,15 +1,22 @@
-# https://leetcode.com/problems/remove-duplicates-from-sorted-list/
-# https://leetcode.com/problems/remove-duplicates-from-sorted-list/discuss/2857882/Python-or-O(n)-O(1)-95.81-with-simplest-solution
-# O(n) O(1)
-class Solution:
+"""
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/discuss/2857882/Python-or-O(n)-O(1)-95.81-with-simplest-solution
+"""
 
-  def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    if head == None:
-      return head
-    curr = head
-    while curr.next:
-      if curr.val == curr.next.val:
-        curr.next = curr.next.next
-      else:
-        curr = curr.next
+from typing import Optional
+from .. import ListNode
+
+
+def delete_duplicates(head: Optional[ListNode]) -> Optional[ListNode]:
+  """
+  O(n) O(1)
+  """
+  if head is None:
     return head
+  curr = head
+  while curr.next:
+    if curr.val == curr.next.val:
+      curr.next = curr.next.next
+    else:
+      curr = curr.next
+  return head
