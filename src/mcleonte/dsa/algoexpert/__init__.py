86e1b7478912662fa1f0a94c91e166346031f169
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:03 +0200

    add BinaryTree

--- src/mcleonte/dsa/algoexpert/__init__.py
+++ src/mcleonte/dsa/algoexpert/__init__.py
@@ -8,3 +8,11 @@ class AncestralTree:
   def __init__(self, name):
     self.name = name
     self.ancestor = None
+
+
+class BinaryTree:
+
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
