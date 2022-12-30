35a1c615c8f1981b0d073207cbf86b6eadfdf802
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/actors_and_directors_who_cooperated_at_least_three_times.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/actors_and_directors_who_cooperated_at_least_three_times.sql
@@ -1,8 +1,8 @@
 -- https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
 SELECT
-    actor_id,
-    director_id
+  actor_id,
+  director_id
 FROM
-    ActorDirector
-GROUP BY 1,2
+  actordirector
+GROUP BY 1, 2
 HAVING COUNT(timestamp) >= 3
