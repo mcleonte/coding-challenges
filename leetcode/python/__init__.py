cd537b935cae4c9cc0c47a911fec08f972fd1b50
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    add ListNode

--- leetcode/python/__init__.py
+++ leetcode/python/__init__.py
@@ -4,7 +4,15 @@ Helper classes for validating type hints.
 
 
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+class ListNode:
+
+  def __init__(self, val=0, nxt=None):
+    self.val = val
+    self.next = nxt
