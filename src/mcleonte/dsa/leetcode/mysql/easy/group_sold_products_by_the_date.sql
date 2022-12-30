c28e49d705f88a8706593236e3c6f572e76949b6
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/group_sold_products_by_the_date.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/group_sold_products_by_the_date.sql
@@ -1,8 +1,8 @@
 -- https://leetcode.com/problems/group-sold-products-by-the-date/
 SELECT
-    sell_date,
-    COUNT(DISTINCT product) AS num_sold,
-    GROUP_CONCAT(DISTINCT product ORDER BY product) AS products
-FROM Activities
-GROUP BY 1
-ORDER BY 1
+  sell_date,
+  COUNT(DISTINCT product) AS num_sold,
+  GROUP_CONCAT(DISTINCT product ORDER BY product) AS products
+FROM activities
+GROUP BY sell_date
+ORDER BY sell_date
