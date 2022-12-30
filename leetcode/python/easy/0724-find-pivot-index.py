516a174e841ec10454005469eb17b0dc8b03306e
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:01 +0200

    init commit

---
+++ leetcode/python/easy/0724-find-pivot-index.py
@@ -0,0 +1,13 @@
+# Solution complexity: O(n) O(1)
+# Description: https://leetcode.com/problems/find-pivot-index/
+# Thread: https://leetcode.com/problems/find-pivot-index/discuss/2815043/Python-or-O(n)-O(1)-or-very-clean
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        lsum = 0
+        rsum = sum(nums)
+        for i, n in enumerate(nums):
+            rsum -= n
+            if lsum == rsum:
+                return i
+            lsum += n
+        return -1
