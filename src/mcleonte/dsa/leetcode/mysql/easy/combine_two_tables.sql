9c3ae81f61f2c84553a00bbcba77b149344740ff
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/combine_two_tables.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/combine_two_tables.sql
@@ -1,10 +1,10 @@
 -- https://leetcode.com/problems/combine-two-tables/
 -- https://leetcode.com/problems/combine-two-tables/discuss/2831219/MySQL-or-clean-solution-without-table-aliases
 SELECT
-    firstName,
-    lastName,
-    city,
-    state
-FROM Person
-LEFT JOIN Address
-USING(personId)
+  person.firstname,
+  person.lastname,
+  address.city,
+  address.state
+FROM person
+LEFT JOIN address
+  USING (personid)
