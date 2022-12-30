f6ba1114a1729709f533901506b484a4488597b4
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/daily_leads_and_partners.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/daily_leads_and_partners.sql
@@ -1,8 +1,8 @@
 -- https://leetcode.com/problems/daily-leads-and-partners/submissions/
 SELECT
-    date_id,
-    make_name,
-    COUNT(DISTINCT lead_id) as unique_leads,
-    COUNT(DISTINCT partner_id) as unique_partners
-FROM DailySales
+  date_id,
+  make_name,
+  COUNT(DISTINCT lead_id) AS unique_leads,
+  COUNT(DISTINCT partner_id) AS unique_partners
+FROM dailysales
 GROUP BY date_id, make_name
