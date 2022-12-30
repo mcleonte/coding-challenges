9c642ecf0a9d95f9e767699912687914cf2238e5
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/fix_names_in_a_table.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/fix_names_in_a_table.sql
@@ -3,8 +3,8 @@
 SELECT
   user_id,
   CONCAT(
-    UPPER(LEFT(name,1)),
-		LOWER(RIGHT(name,LENGTH(name)-1))
-	) as name
-FROM Users
+    UPPER(LEFT(name, 1)),
+    LOWER(RIGHT(name, LENGTH(name) - 1))
+  ) AS name
+FROM users
 ORDER BY user_id
