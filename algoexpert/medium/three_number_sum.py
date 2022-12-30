23cd14c46129fa51d255befb37ef6e24960b2685
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:03 +0200

    add docstring

--- algoexpert/medium/three_number_sum.py
+++ algoexpert/medium/three_number_sum.py
@@ -1,13 +1,28 @@
-# O(n^2) O(n)
-def threeNumberSum(array, targetSum):
-  array.sort()  # O(nlogn)
-  out, l = [], len(array)
+"""
+Write a function that takes in a non-empty array of distinct integers and an
+integer representing a target sum. The function should find all triplets in the
+array that sum up to the target sum and return a two-dimensional array of all
+these triplets. The numbers in each triplet should be ordered in ascending
+order, and the triplets themselves should be ordered in ascending order with
+respect to the numbers they hold.
+
+If no three numbers sum up to the target sum, the function should return an
+empty array.
+"""
+
+
+def three_number_sum(arr, tgt):
+  """
+  O(n^2) O(n)
+  """
+  arr.sort()  # O(nlogn)
+  out, l = [], len(arr)
   for i in range(l - 1):
-    j, k, t = i + 1, l - 1, targetSum - array[i]
+    j, k, t = i + 1, l - 1, tgt - arr[i]
     while j < k:
-      s = array[j] + array[k]
+      s = arr[j] + arr[k]
       if s == t:
-        out.append([array[i], array[j], array[k]])
+        out.append([arr[i], arr[j], arr[k]])
         j += 1
         k -= 1
       elif s < t:
@@ -17,15 +32,17 @@ def threeNumberSum(array, targetSum):
   return out
 
 
-# O(n^3) O(n)
-def threeNumberSum(array, targetSum):
-  array.sort()  # O(nlogn)
-  out, l = [], len(array)
+def three_number_sum_2(arr, tgt):
+  """
+  O(n^3) O(n)
+  """
+  arr.sort()  # O(nlogn)
+  out, l = [], len(arr)
   for i in range(l - 2):
-    t = targetSum - array[i]
+    t = tgt - arr[i]
     for j in range(i + 1, l - 1):
-      t2 = t - array[j]
+      t2 = t - arr[j]
       for k in range(j + 1, l):
-        if array[k] == t2:
-          out.append([array[i], array[j], array[k]])
+        if arr[k] == t2:
+          out.append([arr[i], arr[j], arr[k]])
   return out
