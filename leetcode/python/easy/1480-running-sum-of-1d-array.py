b27847816ed8111c1876d86ee8f7d87d8c1ea8fd
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    reverse comment lines

--- leetcode/python/easy/1480-running-sum-of-1d-array.py
+++ leetcode/python/easy/1480-running-sum-of-1d-array.py
@@ -1,5 +1,5 @@
-# https://leetcode.com/problems/running-sum-of-1d-array/
 # O(n) O(1)
+# https://leetcode.com/problems/running-sum-of-1d-array/
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         for i in range(1, len(nums)):
