6ad6503335c0188494ffcd4e0431d46a534c663d
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/customer_placing_the_largest_number_of_orders.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/customer_placing_the_largest_number_of_orders.sql
@@ -1,6 +1,6 @@
 -- https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/
 SELECT customer_number
-FROM Orders
+FROM orders
 GROUP BY customer_number
 ORDER BY COUNT(order_number) DESC
 LIMIT 1
