a1793db5577313f7df36bc9ecae6d29e1a1c6966
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/the_latest_login_in_2020.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/the_latest_login_in_2020.sql
@@ -1,8 +1,8 @@
 -- https://leetcode.com/problems/the-latest-login-in-2020/
 -- https://leetcode.com/problems/the-latest-login-in-2020/discuss/2828564/MySQL-or-greater96
 SELECT
-    user_id,
-    MAX(time_stamp) as last_stamp
-FROM Logins
-WHERE YEAR(time_stamp)=2020
+  user_id,
+  MAX(time_stamp) AS last_stamp
+FROM logins
+WHERE YEAR(time_stamp) = 2020
 GROUP BY user_id
