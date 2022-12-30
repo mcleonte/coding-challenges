cd3cafe0c541ac458854d84135e947b4fe102bfe
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:01 +0200

    add discuss link

--- leetcode/mysql/easy/1141-user-activity-for-the-past-30-days-i.sql
+++ leetcode/mysql/easy/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,4 +1,5 @@
 -- https://leetcode.com/problems/user-activity-for-the-past-30-days-i/
+-- https://leetcode.com/problems/user-activity-for-the-past-30-days-i/discuss/2825169/MySQL-or-Single-WHERE-clause%3A-DATEDIFF-...-BETWEEN-0-AND-29
 SELECT
     activity_date as day,
     COUNT(DISTINCT user_id) as active_users
