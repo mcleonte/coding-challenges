509852022c6a6d5e64f184f0fd202330817871e2
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:03 +0200

    improve formatting and naming

--- src/mcleonte/dsa/leetcode/python/medium/best_time_to_buy_and_sell_stock_with_cooldown.py
+++ src/mcleonte/dsa/leetcode/python/medium/best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -1,4 +1,11 @@
 """
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/2946408/Python-or-O(n)-O(1)-greater99-or-easy-to-understand-with-good-formatting-and-state-naming
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/2940485/Daily-LeetCoding-Challenge-December-Day-23/1726630
+
+
 You are given an array prices where prices[i] is the price of a given stock on
 the ith day.
 
@@ -14,3 +21,24 @@ sell the stock before you buy again).
 """
 
 from typing import List
+
+
+def max_profit(prices: List[int]) -> int:
+  """
+  O(n) O(1)
+  """
+  holding_on, holding_off, cooling_off = float('-inf'), 0, 0
+
+  for price in prices:
+    holding_on, holding_off, cooling_off = \
+      max(
+        holding_on,           # holding_on  --> wait --> holding_on
+        holding_off - price,  # holding_off --> buy  --> holding_on
+      ), \
+      max(
+        cooling_off,          # cooling_off --> wait --> holding_off
+        holding_off,          # holding_off --> wait --> holding_off
+      ), \
+      holding_on + price      # holding_on  --> sell --> cooling_off
+
+  return max(cooling_off, holding_off)
