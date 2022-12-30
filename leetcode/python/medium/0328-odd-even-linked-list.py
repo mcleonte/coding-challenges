f4501dcf0c59d8a7f4884256957d1145bd019e77
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    init commit

---
+++ leetcode/python/medium/0328-odd-even-linked-list.py
@@ -0,0 +1,53 @@
+"""
+https://leetcode.com/problems/odd-even-linked-list/
+https://leetcode.com/problems/odd-even-linked-list/solutions/2881889/python-o-n-o-1-short-lbyl-and-eafp-examples/
+https://leetcode.com/problems/odd-even-linked-list/solutions/2881668/daily-leetcoding-challenge-december-day-6/comments/1706876
+"""
+
+from typing import Optional
+
+
+class ListNode:
+
+    def __init__(self, val=0, nxt=None):
+        self.val = val
+        self.next = nxt
+
+
+class Solution:
+    """
+    O(n) O(1)
+    """
+
+    # EAFP
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        odd, even, even_head = head, head.next, head.next
+        while True:
+            try:
+                prev_odd = odd
+                odd.next = odd = even.next
+                even.next = even = odd.next
+            except AttributeError:
+                break
+        odd = odd or prev_odd
+        odd.next = even_head
+        return head
+
+    # LBYL
+    def oddEvenList2(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        odd, even, even_head = head, head.next, head.next
+        while True:
+            if not even:
+                break
+            prev_odd = odd
+            odd.next = odd = even.next
+            if not odd:
+                odd = prev_odd
+                break
+            even.next = even = odd.next
+        odd.next = even_head
+        return head
