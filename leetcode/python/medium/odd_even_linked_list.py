72edd521361e6682ec6772fe7fc7f13099052b04
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    change style

--- leetcode/python/medium/odd_even_linked_list.py
+++ leetcode/python/medium/odd_even_linked_list.py
@@ -9,40 +9,42 @@ from typing import Optional
 from .. import ListNode
 
 
-class Solution:
+def odd_even_list(head: Optional[ListNode]) -> Optional[ListNode]:
   """
-    O(n) O(1)
-    """
-
-  # EAFP
-  def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    if not head:
-      return head
-    odd, even, even_head = head, head.next, head.next
-    while True:
-      try:
-        prev_odd = odd
-        odd.next = odd = even.next
-        even.next = even = odd.next
-      except AttributeError:
-        break
-    odd = odd or prev_odd
-    odd.next = even_head
+  EAFP style
+  O(n) O(1)
+  """
+  if not head:
     return head
-
-  # LBYL
-  def oddEvenList2(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    if not head:
-      return head
-    odd, even, even_head = head, head.next, head.next
-    while True:
-      if not even:
-        break
+  odd, even, even_head = head, head.next, head.next
+  while True:
+    try:
       prev_odd = odd
       odd.next = odd = even.next
-      if not odd:
-        odd = prev_odd
-        break
       even.next = even = odd.next
-    odd.next = even_head
+    except AttributeError:
+      break
+  odd = odd or prev_odd
+  odd.next = even_head
+  return head
+
+
+def odd_even_list_2(head: Optional[ListNode]) -> Optional[ListNode]:
+  """
+  LBYL
+  O(n) O(1)
+  """
+  if not head:
     return head
+  odd, even, even_head = head, head.next, head.next
+  while True:
+    if not even:
+      break
+    prev_odd = odd
+    odd.next = odd = even.next
+    if not odd:
+      odd = prev_odd
+      break
+    even.next = even = odd.next
+  odd.next = even_head
+  return head
