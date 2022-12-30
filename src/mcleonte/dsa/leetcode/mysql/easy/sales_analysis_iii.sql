eae7942cdf0a6e2e488bc972b6a7a8e3babad874
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/sales_analysis_iii.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/sales_analysis_iii.sql
@@ -1,8 +1,10 @@
 -- https://leetcode.com/problems/sales-analysis-iii/
-SELECT product_id, product_name
-FROM Product
-JOIN Sales
-USING(product_id)
+SELECT
+  product_id,
+  product.product_name
+FROM product
+INNER JOIN sales
+  USING (product_id)
 GROUP BY product_id
-HAVING MIN(sale_date) >= '2019-01-01'
-   AND MAX(sale_date) <= '2019-03-31'
+HAVING MIN(sales.sale_date) >= '2019-01-01'
+  AND MAX(sales.sale_date) <= '2019-03-31'
