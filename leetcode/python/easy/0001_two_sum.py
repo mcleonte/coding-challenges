703a95557cabaaf572ed4b5b41099e77f368f7d8
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    pylint: fix style

--- leetcode/python/easy/0001_two_sum.py
+++
@@ -1,8 +0,0 @@
-# https://leetcode.com/problems/two-sum/
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        s = {}
-        for i, n in enumerate(nums):
-            if target - n in s:
-                return [s[target - n], i]
-            s[n] = i
---
+++ leetcode/python/easy/two_sum.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode.com/problems/two-sum/
+
+Given an array of integers nums and an integer target, return indices of the two
+numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not
+use the same element twice.
+
+You can return the answer in any order.
+"""
+
+from typing import List
+
+
+def two_sum(nums: List[int], tgt: int) -> List[int]:
+    """
+    O(n) O(n)
+    """
+    mem = {}
+    for i, num in enumerate(nums):
+        if tgt - num in mem:
+            return [mem[tgt - num], i]
+        mem[num] = i
+    return None
