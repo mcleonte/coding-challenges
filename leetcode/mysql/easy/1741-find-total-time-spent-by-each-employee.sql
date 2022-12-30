86645e5680dfb3c63e99d3ff89355efc5ba25a3c
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:01 +0200

    init commit

---
+++ leetcode/mysql/easy/1741-find-total-time-spent-by-each-employee.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/find-total-time-spent-by-each-employee/
+SELECT
+    event_day as day,
+    emp_id,
+    SUM(out_time - in_time) as total_time
+FROM Employees
+GROUP BY emp_id, event_day
