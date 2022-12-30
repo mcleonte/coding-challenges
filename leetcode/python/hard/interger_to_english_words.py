f7e478f395aa03569855628178db9d4cc51d17d6
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    pylint:fix style

--- leetcode/python/hard/interger_to_english_words.py
+++ leetcode/python/hard/interger_to_english_words.py
@@ -1,71 +1,81 @@
-# https://leetcode.com/problems/integer-to-english-words/
-class Solution:
+"""
+https://leetcode.com/problems/integer-to-english-words/
 
-    d = {  # digits
-        "1": "One",
-        "2": "Two",
-        "3": "Three",
-        "4": "Four",
-        "5": "Five",
-        "6": "Six",
-        "7": "Seven",
-        "8": "Eight",
-        "9": "Nine",
-    }
-    e = {  # edge cases
-        "2": "Twenty",
-        "3": "Thirty",
-        "4": "Forty",
-        "5": "Fifty",
-        "8": "Eighty",
-        "10": "Ten",
-        "11": "Eleven",
-        "12": "Twelve",
-        "13": "Thirteen",
-        "14": "Fourteen",
-        "15": "Fifteen",
-        "16": "Sixteen",
-        "17": "Seventeen",
-        "18": "Eighteen",
-        "19": "Nineteen",
-    }
+Convert a non-negative integer num to its English words representation.
+"""
 
-    o = {2: "Thousand", 3: "Million", 4: "Billion"}  # orders
+DIGITS = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+}
+EDGE_CASES = {
+    "2": "Twenty",
+    "3": "Thirty",
+    "4": "Forty",
+    "5": "Fifty",
+    "8": "Eighty",
+    "10": "Ten",
+    "11": "Eleven",
+    "12": "Twelve",
+    "13": "Thirteen",
+    "14": "Fourteen",
+    "15": "Fifteen",
+    "16": "Sixteen",
+    "17": "Seventeen",
+    "18": "Eighteen",
+    "19": "Nineteen",
+}
+ORDERS = {
+    2: "Thousand",
+    3: "Million",
+    4: "Billion",
+}
 
-    def numberToWords(self, num: int) -> str:
-        d, e, o = self.d, self.e, self.o
-        n = str(num)
-        n = "0" * (0, 2, 1)[len(n) % 3] + n
-        out = []
-        threes = [n[i : i + 3] for i in range(0, len(n), 3)]
-        for i, (h, t, u) in enumerate(threes):
+
+def number_to_words(num: int) -> str:
+    """
+    O(n) O(n) | n = len(str(num))
+    """
+    num = str(num)
+    num = "0" * (0, 2, 1)[len(num) % 3] + num  # ensure 3 digits string
+    out = []
+    triplets = [num[i : i + 3] for i in range(0, len(num), 3)]
+
+    for i, (hundreds, tens, units) in enumerate(triplets):
+
+        try:
+            out.append(DIGITS[hundreds])
+        except KeyError:
+            pass
+
+        try:
+            out.append(EDGE_CASES[tens + units])
+        except KeyError:
 
             try:
-                out += d[h], "Hundred"
+                out.append(EDGE_CASES[tens])
+            except KeyError:
+                try:
+                    out.append(DIGITS[tens] + "ty")
+                except KeyError:
+                    pass
+
+            try:
+                out.append(DIGITS[units])
             except KeyError:
                 pass
 
+        if hundreds + tens + units != "000":
             try:
-                out.append(e[t + u])
+                out.append(ORDERS[len(triplets) - i])
             except KeyError:
+                pass
 
-                try:
-                    out.append(e[t])
-                except KeyError:
-                    try:
-                        out.append(d[t] + "ty")
-                    except KeyError:
-                        pass
-
-                try:
-                    out.append(d[u])
-                except KeyError:
-                    pass
-
-            if h + t + u != "000":
-                try:
-                    out.append(o[len(threes) - i])
-                except KeyError:
-                    pass
-
-        return " ".join(out) or "Zero"
+    return " ".join(out) or "Zero"
