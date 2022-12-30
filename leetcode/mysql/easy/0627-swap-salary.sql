447cfc96f6e48aeec7014e01bb4ccededc017348
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    init commit

---
+++ leetcode/mysql/easy/0627-swap-salary.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/swap-salary/
+-- https://leetcode.com/problems/swap-salary/discuss/2837455/MySQL-or-98.92-222m-with-shortest-solution
+UPDATE Salary
+SET sex = IF(sex='f','m','f')
