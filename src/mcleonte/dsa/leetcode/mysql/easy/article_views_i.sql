dedbf3c423196c185e2058e236b41698e0451362
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/article_views_i.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/article_views_i.sql
@@ -1,6 +1,6 @@
 -- https://leetcode.com/problems/article-views-i/
 -- https://leetcode.com/problems/article-views-i/discuss/2825204/MySQL-or-greater95-with-simplest-approach
 SELECT DISTINCT author_id AS id
-FROM Views
+FROM views
 WHERE author_id = viewer_id
 ORDER BY id
