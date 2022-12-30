dcc50112416841cbe7b970163a7a9b15dda364c6
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    pylint: fix style

--- algoexpert/easy/binary_search.py
+++ algoexpert/easy/binary_search.py
@@ -1,12 +1,25 @@
-# O(log(n)) O(1)
-def binarySearch(a, t):
-    l, r = 0, len(a) - 1
-    while l <= r:
-        m = (l + r) // 2
-        if t == a[m]:
-            return m
-        if t > a[m]:
-            l = m + 1
+"""
+Write a function that takes in a sorted array of integers as well as a target
+integer. The function should use the Binary Search algorithm to determine if the
+target integer is contained in the array and should return its index if it is,
+otherwise -1.
+
+If you're unfamiliar with Binary Search, we recommend watching the conceptual
+Overview section of this question's video explanation before starting to code.
+"""
+
+
+def binary_search(arr, tgt):
+    """
+    O(logn) O(1)
+    """
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if tgt == arr[mid]:
+            return mid
+        if tgt > arr[mid]:
+            left = mid + 1
         else:
-            r = m - 1
+            right = mid - 1
     return -1
