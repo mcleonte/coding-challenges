8f7329fb93e7083f2dcffbbd6997493390940da8
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:04 +0200

    fix formatting

--- src/mcleonte/dsa/leetcode/mysql/easy/employees_with_missing_information.sql
+++ src/mcleonte/dsa/leetcode/mysql/easy/employees_with_missing_information.sql
@@ -5,22 +5,23 @@
 
 WITH full_join AS (
 
-    SELECT *
-    FROM Employees
-    LEFT JOIN Salaries
-    USING(employee_id)
+  SELECT *
+  FROM employees
+  LEFT JOIN salaries
+    USING (employeed_id)
 
-    UNION ALL
+  UNION ALL
 
-    SELECT *
-    FROM Employees
-    RIGHT JOIN Salaries
-    USING(employee_id)
+  SELECT *
+  FROM salaries
+  LEFT JOIN employees
+    USING (employeed_id)
 )
+
 SELECT employee_id
 FROM full_join
 WHERE name IS NULL
-OR salary IS NULL
+  OR salary IS NULL
 ORDER BY employee_id;
 
 -- v2 - subquery
@@ -28,39 +29,40 @@ ORDER BY employee_id;
 SELECT employee_id
 FROM (
 
-    SELECT *
-    FROM Employees
-    LEFT JOIN Salaries
-    USING(employee_id)
+  SELECT *
+  FROM employees
+  LEFT JOIN salaries
+    USING (employeed_id)
 
-    UNION ALL
+  UNION ALL
 
-    SELECT *
-    FROM Employees
-    RIGHT JOIN Salaries
-    USING(employee_id)
+  SELECT *
+  FROM salaries
+  LEFT JOIN employees
+    USING (employeed_id)
 
 ) AS full_join
+
 WHERE name IS NULL
-OR salary IS NULL
+  OR salary IS NULL
 ORDER BY employee_id;
 
 -- v3 - filter each join before union
 
 SELECT employee_id
-FROM Employees
-LEFT JOIN Salaries
-USING(employee_id)
-WHERE name IS NULL
-OR salary IS NULL
+FROM employees
+LEFT JOIN salaries
+  USING (employee_id)
+WHERE employees.name IS NULL
+  OR salaries.salary IS NULL
 
 UNION ALL
 
 SELECT employee_id
-FROM Employees
-RIGHT JOIN Salaries
-USING(employee_id)
-WHERE name IS NULL
-OR salary IS NULL
+FROM salaries
+LEFT JOIN employees
+  USING (employee_id)
+WHERE employees.name IS NULL
+  OR salaries.salary IS NULL
 
 ORDER BY employee_id;
