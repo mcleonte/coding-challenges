6d5778f7145aec50c6cb02882921979d4f4b16d3
Mihail Costin Leonte <git@mcleonte.com>
2022-12-30 03:12:02 +0200

    fix pylint

---
+++ algoexpert/hard/longest_substring_without_duplication.py
@@ -0,0 +1,21 @@
+"""
+Write a function that takes in a string and returns its longest substring
+without duplicate characters.
+
+You can assume that there will only be one longest substring without duplication.
+"""
+
+
+def longest_substring_without_duplication(string):
+    """
+    O(n) O(s) | n = len(string), s = len(set(string))
+    """
+    mem = {}
+    i = out_i = out_j = 0
+    for j, char in enumerate(string):
+        if char in mem:
+            i = max(i, mem[char] + 1)
+        if j - i > out_j - out_i:
+            out_i, out_j = i, j
+        mem[char] = j
+    return string[out_i : out_j + 1]
