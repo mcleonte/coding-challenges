03062f89edeeca034111281393f5fd1100225146
Mihail Costin Leonte <git@mcleonte.com>
2023-01-12 00:24:06 +0200

    add links and refactor solution

--- src/mcleonte/dsa/leetcode/python/medium/minimum_time_to_collect_all_apples_in_a_tree.py
+++ src/mcleonte/dsa/leetcode/python/medium/minimum_time_to_collect_all_apples_in_a_tree.py
@@ -1,4 +1,11 @@
 """
+https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/
+
+https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/discuss/3037713/Python-or-O(n)-O(n)-or-2-liner-DFS-inner-function
+
+https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/discuss/3032890/Daily-LeetCoding-Challenge-January-Day-11/1753294
+
+
 Given an undirected tree consisting of n vertices numbered from 0 to n-1, which
 has some apples in their vertices. You spend 1 second to walk over one edge of
 the tree. Return the minimum time in seconds you have to spend to collect all
@@ -14,18 +21,17 @@ from typing import List
 from collections import defaultdict
 
 
-def min_time(
-    edges: List[List[int]],
-    has_apple: List[bool],
-) -> int:
+def min_time(edges: List[List[int]], has_apple: List[bool]) -> int:
+  """
+  O(n) O(n)
+  """
+
+  def dfs(node: int = 0, prev: int = -1) -> int:
+    time = sum(dfs(adj, node) for adj in adjs[node] if adj != prev)
+    return time + 1 if time else has_apple[node]
 
   adjs = defaultdict(list)
   for a, b in edges:
     adjs[a].append(b)
     adjs[b].append(a)
-
-  def dfs(node: int = 0, prev: int = -1) -> int:
-    res = sum(dfs(adj, node) for adj in adjs[node] if adj != prev)
-    return res + 1 if res else has_apple[node]
-
-  return 2 * max(0, dfs() - 1)
+  return max(0, 2 * dfs() - 2)
