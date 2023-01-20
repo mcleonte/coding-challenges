76fa4cb63fd3ede9bf8831598801d60488ec6535
Mihail Costin Leonte <git@mcleonte.com>
2023-01-20 23:25:35 +0200

    init commit

---
+++ src/mcleonte/dsa/leetcode/python/easy/convert_the_temperature.py
@@ -0,0 +1,15 @@
+"""
+You are given a non-negative floating point number rounded to two decimal places
+celsius, that denotes the temperature in Celsius.
+
+You should convert Celsius into Kelvin and Fahrenheit and return it as an array
+ans = [kelvin, fahrenheit].
+
+Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+"""
+
+from typing import List
+
+
+def convert_temperature(celsius: float) -> List[float]:
+  return [celsius + 273.15, celsius * 1.8 + 32.]
